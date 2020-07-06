247abc1f67293e992d143d341b213bedab997bde
Erik Navarrete <leonavas1995@gmail.com>
2020-07-06 22:09:03 +0200

    publish

---
+++ docs/precache-manifest.7a6513d402def0f3db78af8916b7acdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d50a5b559e7fec5ee9c2b838127a2d0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "901466439ab4976c3fd1",
+    "url": "/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "aefacb34f8a6e3a8af51",
+    "url": "/static/css/main.cf0832a3.chunk.css"
+  },
+  {
+    "revision": "901466439ab4976c3fd1",
+    "url": "/static/js/2.aeb47059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.aeb47059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aefacb34f8a6e3a8af51",
+    "url": "/static/js/main.4d816d55.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
